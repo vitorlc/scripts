68fc276201b1897dfe37ce305fc97dab3577bedf
vitorlc <vitor_cirqueira1@hotmail.com>
2019-09-06 15:21:14 -0300

    Add v1 script linux setup

---
+++ my_linux_setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Vitor's Basic Development Machine Setup
+# Author : Vitor
+
+sudo apt-get update
+
+# Install basic linux tools and utilities
+sudo apt-get install -y libssl-dev
+sudo apt-get install build-essential \ libffi-dev python-dev
+
+# Git - a version control system
+sudo apt-get update
+sudo apt-get install -y git xclip
+
+# Vim, Curl, Python - Some random useful stuff
+sudo apt-get install -y vim curl python-software-properties
+sudo apt-get install -y python-dev, python-pip
+sudo apt-get install -y libkrb5-dev
+
+# Nodejs
+curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+sudo apt-get install nodejs
+
+# Installing JDK and JRE
+sudo apt-get install -y default-jre
+sudo apt-get install -y default-jdk
+
+# Gimp Image Editor
+sudo apt-get install -y gimp gimp-data gimp-plugin-registry gimp-data-extras
+
+# Archive Extractors
+sudo apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller
+
+# TLP - saves battery when Ubuntu is installed on Laptops
+sudo apt-get remove laptop-mode-tools
+sudo add-apt-repository ppa:linrunner/tlp
+sudo apt-get update
+sudo apt-get install -y tlp tlp-rdw smartmontools ethtool
+sudo tlp start
+sudo tlp stat
+
+# KVM acceleration and cpu checker
+sudo apt-get install -y cpu-checker
+sudo apt-get install -y qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
+sudo apt-get install -y virt-manager
+sudo apt-get install -y lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
+
+# Calibre - Ebook reader and converter
+sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+
+# Zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
