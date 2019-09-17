a1b7ea2a503c2ee38b99494851ab62c3f0380def
vitorlc <vitor_cirqueira1@hotmail.com>
2019-09-16 21:35:16 -0300

    Add yarn

--- my_linux_setup.sh
+++ my_linux_setup.sh
@@ -2,40 +2,41 @@
 # Vitor's Basic Development Machine Setup
 # Author : Vitor
 
-sudo apt-get update
+sudo apt update
 
 # Install basic linux tools and utilities
-sudo apt-get install -y libssl-dev
-sudo apt-get install build-essential \ libffi-dev python-dev
+sudo apt install -y libssl-dev
+sudo apt install build-essential \ libffi-dev python-dev
 
 # Git - a version control system
-sudo apt-get update
-sudo apt-get install -y git xclip
+sudo apt update
+sudo apt install -y git xclip
 
 # Install Snap
-sudo apt-get install -y snapd snapd-xdg-open
+sudo apt install -y snapd snapd-xdg-open
 
 # Install Code
 sudo snap install --classic code 
 
 # Vim, Curl, Python - Some random useful stuff
-sudo apt-get install -y vim curl python-software-properties
-sudo apt-get install -y python-dev, python-pip
-sudo apt-get install -y libkrb5-dev
+sudo apt install -y vim curl python-software-properties
+sudo apt install -y python-dev, python-pip
+sudo apt install -y libkrb5-dev
 
-# Nodejs
+# Nodejs, Npm and Yarn
 curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
-sudo apt-get install nodejs
+sudo apt install nodejs
+sudo apt install yarn
 
 # Installing JDK and JRE
-sudo apt-get install -y default-jre
-sudo apt-get install -y default-jdk
+sudo apt install -y default-jre
+sudo apt install -y default-jdk
 
 # Gimp Image Editor
-sudo apt-get install -y gimp gimp-data gimp-plugin-registry gimp-data-extras
+sudo apt install -y gimp gimp-data gimp-plugin-registry gimp-data-extras
 
 # Archive Extractors
-sudo apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller
+sudo apt install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller
 
 # Zsh
 sudo apt install zsh
