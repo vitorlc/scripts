80043161f953292ce1c4262aacd7cfb5efdc6875
vitorlc <vitor_cirqueira1@hotmail.com>
2019-10-18 19:29:44 -0300

    Add script my apps

---
+++ my_apps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Vitor's Apps
+# Author : Vitor
+
+# Spotify
+sudo snap install spotify
+
+# Slack
+sudo snap install slack --classic
+
+# Steam
+sudo apt install -y steam
+
+# Gimp Image Editor
+sudo apt install -y gimp gimp-data gimp-plugin-registry gimp-data-extras
+
+# Vlc - Media Player
+sudo snap install vlc
